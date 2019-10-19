0e7fc99b28afe3ed6472f989083e8dcf46dd83d5
g.piepoli <g.piepoli@evolware.it>
2019-10-19 09:48:16 +0200

    Created stats page

---
+++ fb-stats.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<!-- Just an image -->
+
+<nav class="navbar navbar-expand-lg navbar-light discount-background">
+    <a class="navbar-brand" href="#">
+        <img src="http://localhost/facebooksysag/upload/discounts.jpg" width="90" height="30" alt="">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="fb-stats.php">Vedi Statistiche</a>
+        </li>
+        </ul>
+        <span class="navbar-text"></span>
+    </div>
+</nav>
+
+
+<!-- INSERT CONTAINER -->
+
+
+<footer class="footer discount-background">
+    <div class="container" style="max-width: inherit;">
+        <div class="row" style="padding:2%; color:white;">
+            <div class="col-12">
+                <p>Copyright 2019 - DGM Group</p>
+            </div>
+        </div>
+    </div>
+<footer>
+
+<script src="/facebooksysag/lib/chart/Chart.min.js"></script>
+<script src="/facebooksysag/lib/jquery/jquery-3.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
